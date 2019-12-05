68a16ae2bc4f4439358dfbd7f41293d0fb66f319
Zeram <zeram3@gmail.com>
2019-12-05 13:03:52 -0400

    Update rdpwrap-uninstall-install.bat

--- rdpwrap-uninstall-install.bat
+++ rdpwrap-uninstall-install.bat
@@ -12,6 +12,9 @@ echo [-] Installer executable not found.
 echo Please extract all files from the downloaded package or check your anti-virus.
 :anykey
 
+@REM https://stackoverflow.com/questions/25648155/windows-command-line-to-read-version-info-of-an-executable-file
+wmic datafile where name="C:\\Windows\\System32\\termsrv.dll" get Version /value
+
 @REM http://www.openmutual.org/2011/08/gnu-wget-missing-libeay32-dll-on-new-install-on-windows/
 @REM https://redfern.me/skipping-certificate-checks-with-wget/
 @REM https://serverfault.com/questions/171369/how-to-allow-wget-to-overwrite-files
